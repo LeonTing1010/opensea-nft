8c9b8b512acee5f3530f7ff4c78d11ef57374f05
Leon <zhongchang.ding@gmail.com>
2022-07-29 15:24:53 +0800

    scripts of lottery

--- nft-contract/scripts/welfare.js
+++ nft-contract/scripts/welfare.js
@@ -1,22 +1,100 @@
 const { task } = require("hardhat/config");
-const { getContract, getEnvVariable } = require("./helpers");
+const { getContract, getAccount, getEnvVariable } = require("./helpers");
 const ContractName = "WelfareFoctory";
 const ContractKey = "WELFAREFOCTORY_CONTRACT_ADDRESS";
 
 task("newLottery", "New a  Lottery")
-  .addParam("len", "The length of lottery")
+  .addParam("l", "The length of lottery")
   .setAction(async function (taskArguments, hre) {
-    const contract = await getContract(getEnvVariable(ContractKey), ContractName, hre);
-    const lotteryTx = await contract.newLottery(taskArguments.len, {
-      gasLimit: 500_000,
+    const factory = getEnvVariable(ContractKey);
+    const contract = await getContract(factory, ContractName, hre);
+    console.log(`WelfareFoctoryContract address=> ${factory}`);
+    const lotteryTx = await contract.newLottery(taskArguments.l, {
+      gasLimit: 5_000_000,
+      gasPrice: 100_000_000_000,
     });
     await lotteryTx.wait();
     const phase = await contract.phase();
+    console.log("Phase of Lottery=> " + phase);
     const lottery = await contract.getLottery(phase);
     console.log("New Lottery=> " + lottery);
-    console.log("Consumer=> " + (await contract.randomNumberGenerator()));
+    const contractLottery = await getContract(lottery, "Lottery", hre);
+    console.log("Consumer=> " + (await contractLottery.randomNumberGenerator()));
     await hre.run("verify:verify", {
-      address: lottery,
+      address: contractLottery.address,
       constructorArguments: [phase, 1162, "0x7a1bac17ccc5b313516c5e16fb24f7659aa5ebed", "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f"],
     });
+  });
+task("verifyLottery", "Verify the  Lottery")
+  .addParam("phase", "The phase of lottery")
+  .setAction(async function (taskArguments, hre) {
+    const factory = getEnvVariable(ContractKey);
+    const contract = await getContract(factory, ContractName, hre);
+    console.log(`WelfareFoctoryContract address=> ${factory}`);
+    // const lotteryTx = await contract.newLottery(taskArguments.l, {
+    //   gasLimit: 5_000_000,
+    //   gasPrice: 100_000_000_000,
+    // });
+    // await lotteryTx.wait();
+    const phase = taskArguments.phase;
+    console.log("Phase of Lottery=> " + phase);
+    const lottery = await contract.getLottery(phase);
+    console.log("New Lottery=> " + lottery);
+    const contractLottery = await getContract(lottery, "Lottery", hre);
+    console.log("Consumer=> " + (await contractLottery.randomNumberGenerator()));
+    await hre.run("verify:verify", {
+      address: contractLottery.address,
+      constructorArguments: [phase, 1162, "0x7a1bac17ccc5b313516c5e16fb24f7659aa5ebed", "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f"],
+    });
+  });
+task("grantLottery", "Grant Lottery")
+  .addParam("phase", "The phase of lottery")
+  .addParam("owner", "The owner of lottery")
+  .setAction(async function (taskArguments, hre) {
+    const factory = getEnvVariable(ContractKey);
+    const contract = await getContract(factory, ContractName, hre);
+    console.log(`WelfareFoctoryContract address=> ${factory}`);
+    // const lotteryTx = await contract.newLottery(taskArguments.l, {
+    //   gasLimit: 5_000_000,
+    //   gasPrice: 100_000_000_000,
+    // });
+    // await lotteryTx.wait();
+    const phase = taskArguments.phase;
+    const lottery = await contract.getLottery(phase);
+    console.log("Lottery=> " + lottery);
+    const contractLottery = await getContract(lottery, "Lottery", hre);
+    console.log("Consumer=> " + (await contractLottery.randomNumberGenerator()));
+    console.log("transferOwnership=> " + (await contractLottery.transferOwnership(taskArguments.owner, { gasLimit: 5_000_000, gasPrice: 100_000_000_000 })).hash);
+    // await hre.run("verify:verify", {
+    //   address: contractLottery.address,
+    //   constructorArguments: [phase, 1162, "0x7a1bac17ccc5b313516c5e16fb24f7659aa5ebed", "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f"],
+    // });
+  });
+task("getLottery", "Get Lottery")
+  .addParam("phase", "The phase of lottery")
+  .setAction(async function (taskArguments, hre) {
+    const factory = getEnvVariable(ContractKey);
+    const contract = await getContract(factory, ContractName, hre);
+    console.log(`WelfareFoctoryContract address=> ${factory}`);
+    const phase = taskArguments.phase;
+    const lottery = await contract.getLottery(phase);
+    console.log("Lottery=> " + lottery);
+    const contractLottery = await getContract(lottery, "Lottery", hre);
+    console.log("Consumer=> " + (await contractLottery.randomNumberGenerator()));
+  });
+
+task("twistLottery", "Get Lottery")
+  .addParam("phase", "The phase of lottery")
+  .setAction(async function (taskArguments, hre) {
+    const factory = getEnvVariable(ContractKey);
+    const contract = await getContract(factory, ContractName, hre);
+    console.log(`WelfareFoctoryContract address=> ${factory}`);
+    const phase = taskArguments.phase;
+    const lottery = await contract.getLottery(phase);
+    console.log("Lottery=> " + lottery);
+    const contractLottery = await getContract(lottery, "Lottery", hre);
+    const player = getAccount();
+    console.log("Consumer=> " + (await contractLottery.randomNumberGenerator()));
+    console.log("twist=> " + (await contractLottery.twist(player.address)).hash);
+    console.log("twist=> " + (await contractLottery.twist(player.address)).hash);
   });
